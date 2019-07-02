dc06a537bd51aa50dd8da2052ab4e194d8fc0dbd
sparkvix <ev1lhat0l0ls@gmail.com>
2019-07-02 12:54:12 -0400

    First Commit

---
+++ slack.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Slack
+sudo cp download16.svg /usr/share/icons/Papirus/16x16/apps/slack.svg
+sudo cp download22.svg /usr/share/icons/Papirus/22x22/apps/slack.svg
+sudo cp download24.svg /usr/share/icons/Papirus/24x24/apps/slack.svg
+sudo cp download32.svg /usr/share/icons/Papirus/32x32/apps/slack.svg
+sudo cp download48.svg /usr/share/icons/Papirus/48x48/apps/slack.svg
+sudo cp download64.svg /usr/share/icons/Papirus/64x64/apps/slack.svg
+
+# Enpass
+sudo cp ep16.svg /usr/share/icons/Papirus/16x16/panel/enpass-status.svg
+sudo cp ep22.svg /usr/share/icons/Papirus/22x22/panel/enpass-status.svg
+sudo cp ep24.svg /usr/share/icons/Papirus/24x24/panel/enpass-status.svg
