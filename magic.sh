f06045ca1219738caed6ee88b6761f4c363aa0eb
Sparkvix <ev1lhat0l0ls@gmail.com>
2020-06-29 02:12:13 -0400

    Replace hardcoded pathname with variables

--- magic.sh
+++ magic.sh
@@ -2,106 +2,110 @@
 
 # Swap some Papirus-icon-theme icons to Qogir-icon-theme
 
-dir="./themes"
+dir="./themes/Qogir-icon-theme/src"
+papirus="/usr/share/icons/Papirus"
+papirusdark="/usr/share/icons/Papirus-Dark"
 
 [ -d $dir ] || exit
+[ -d $papirus ] || exit
+[ -d $papirusdark ] || exit
 
-sudo cp ./themes/Qogir-icon-theme/src/32/places/default-fileopen.svg /usr/share/icons/Papirus/32x32/places/folder-blue-documents-open.svg
-sudo cp ./themes/Qogir-icon-theme/src/48/places/default-fileopen.svg /usr/share/icons/Papirus/48x48/places/folder-blue-documents-open.svg
-sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-fileopen.svg /usr/share/icons/Papirus/64x64/places/folder-blue-documents-open.svg
+sudo cp $dir/32/places/default-fileopen.svg $papirus/32x32/places/folder-blue-documents-open.svg
+sudo cp $dir/48/places/default-fileopen.svg $papirus/48x48/places/folder-blue-documents-open.svg
+sudo cp $dir/scalable/places/default-fileopen.svg $papirus/64x64/places/folder-blue-documents-open.svg
 
 ## Default Folder
-sudo cp ./themes/Qogir-icon-theme/src/32/places/default-folder.svg /usr/share/icons/Papirus/32x32/places/folder-blue.svg
-sudo cp ./themes/Qogir-icon-theme/src/48/places/default-folder.svg /usr/share/icons/Papirus/48x48/places/folder-blue.svg
-sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-folder.svg /usr/share/icons/Papirus/64x64/places/folder-blue.svg
+sudo cp $dir/32/places/default-folder.svg $papirus/32x32/places/folder-blue.svg
+sudo cp $dir/48/places/default-folder.svg $papirus/48x48/places/folder-blue.svg
+sudo cp $dir/scalable/places/default-folder.svg $papirus/64x64/places/folder-blue.svg
 
 ## Documents
-sudo cp ./themes/Qogir-icon-theme/src/32/places/default-folder-documents.svg /usr/share/icons/Papirus/32x32/places/folder-blue-documents.svg
-sudo cp ./themes/Qogir-icon-theme/src/48/places/default-folder-documents.svg /usr/share/icons/Papirus/48x48/places/folder-blue-documents.svg
-sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-folder-documents.svg /usr/share/icons/Papirus/64x64/places/folder-blue-documents.svg
+sudo cp $dir/32/places/default-folder-documents.svg $papirus/32x32/places/folder-blue-documents.svg
+sudo cp $dir/48/places/default-folder-documents.svg $papirus/48x48/places/folder-blue-documents.svg
+sudo cp $dir/scalable/places/default-folder-documents.svg $papirus/64x64/places/folder-blue-documents.svg
 
 ## Downloads
-sudo cp ./themes/Qogir-icon-theme/src/32/places/default-folder-download.svg /usr/share/icons/Papirus/32x32/places/folder-blue-download.svg
-sudo cp ./themes/Qogir-icon-theme/src/48/places/default-folder-download.svg /usr/share/icons/Papirus/48x48/places/folder-blue-download.svg
-sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-folder-download.svg /usr/share/icons/Papirus/64x64/places/folder-blue-download.svg
+sudo cp $dir/32/places/default-folder-download.svg $papirus/32x32/places/folder-blue-download.svg
+sudo cp $dir/48/places/default-folder-download.svg $papirus/48x48/places/folder-blue-download.svg
+sudo cp $dir/scalable/places/default-folder-download.svg $papirus/64x64/places/folder-blue-download.svg
 
 ## Music
-sudo cp ./themes/Qogir-icon-theme/src/32/places/default-folder-music.svg /usr/share/icons/Papirus/32x32/places/folder-blue-music.svg
-sudo cp ./themes/Qogir-icon-theme/src/48/places/default-folder-music.svg /usr/share/icons/Papirus/48x48/places/folder-blue-music.svg
-sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-folder-music.svg /usr/share/icons/Papirus/64x64/places/folder-blue-music.svg
+sudo cp $dir/32/places/default-folder-music.svg $papirus/32x32/places/folder-blue-music.svg
+sudo cp $dir/48/places/default-folder-music.svg $papirus/48x48/places/folder-blue-music.svg
+sudo cp $dir/scalable/places/default-folder-music.svg $papirus/64x64/places/folder-blue-music.svg
 
 ## Folder Open
-sudo cp ./themes/Qogir-icon-theme/src/32/places/default-folder-open.svg /usr/share/icons/Papirus/32x32/places/folder-blue-open.svg
-sudo cp ./themes/Qogir-icon-theme/src/48/places/default-folder-open.svg /usr/share/icons/Papirus/48x48/places/folder-blue-open.svg
-sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-folder-open.svg /usr/share/icons/Papirus/64x64/places/folder-blue-open.svg
+sudo cp $dir/32/places/default-folder-open.svg $papirus/32x32/places/folder-blue-open.svg
+sudo cp $dir/48/places/default-folder-open.svg $papirus/48x48/places/folder-blue-open.svg
+sudo cp $dir/scalable/places/default-folder-open.svg $papirus/64x64/places/folder-blue-open.svg
 
 ## Pictures
-sudo cp ./themes/Qogir-icon-theme/src/32/places/default-folder-pictures.svg /usr/share/icons/Papirus/32x32/places/folder-blue-pictures.svg
-sudo cp ./themes/Qogir-icon-theme/src/48/places/default-folder-pictures.svg /usr/share/icons/Papirus/48x48/places/folder-blue-pictures.svg
-sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-folder-pictures.svg /usr/share/icons/Papirus/64x64/places/folder-blue-pictures.svg
+sudo cp $dir/32/places/default-folder-pictures.svg $papirus/32x32/places/folder-blue-pictures.svg
+sudo cp $dir/48/places/default-folder-pictures.svg $papirus/48x48/places/folder-blue-pictures.svg
+sudo cp $dir/scalable/places/default-folder-pictures.svg $papirus/64x64/places/folder-blue-pictures.svg
 
 ## Public Share
-sudo cp ./themes/Qogir-icon-theme/src/32/places/default-folder-publicshare.svg /usr/share/icons/Papirus/32x32/places/folder-blue-image-people.svg
-sudo cp ./themes/Qogir-icon-theme/src/48/places/default-folder-publicshare.svg /usr/share/icons/Papirus/48x48/places/folder-blue-image-people.svg
-sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-folder-publicshare.svg /usr/share/icons/Papirus/64x64/places/folder-blue-image-people.svg
+sudo cp $dir/32/places/default-folder-publicshare.svg $papirus/32x32/places/folder-blue-image-people.svg
+sudo cp $dir/48/places/default-folder-publicshare.svg $papirus/48x48/places/folder-blue-image-people.svg
+sudo cp $dir/scalable/places/default-folder-publicshare.svg $papirus/64x64/places/folder-blue-image-people.svg
 
 ## Video
-sudo cp ./themes/Qogir-icon-theme/src/32/places/default-folder-recent.svg /usr/share/icons/Papirus/32x32/places/folder-blue-recent.svg
-sudo cp ./themes/Qogir-icon-theme/src/48/places/default-folder-recent.svg /usr/share/icons/Papirus/48x48/places/folder-blue-recent.svg
-sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-folder-recent.svg /usr/share/icons/Papirus/64x64/places/folder-blue-recent.svg
+sudo cp $dir/32/places/default-folder-recent.svg $papirus/32x32/places/folder-blue-recent.svg
+sudo cp $dir/48/places/default-folder-recent.svg $papirus/48x48/places/folder-blue-recent.svg
+sudo cp $dir/scalable/places/default-folder-recent.svg $papirus/64x64/places/folder-blue-recent.svg
 ## Developemt
-sudo cp ./themes/Qogir-icon-theme/src/32/places/default-folder-system.svg /usr/share/icons/Papirus/32x32/places/folder-blue-development.svg
-sudo cp ./themes/Qogir-icon-theme/src/48/places/default-folder-system.svg /usr/share/icons/Papirus/48x48/places/folder-blue-development.svg
-sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-folder-system.svg /usr/share/icons/Papirus/64x64/places/folder-blue-development.svg
+sudo cp $dir/32/places/default-folder-system.svg $papirus/32x32/places/folder-blue-development.svg
+sudo cp $dir/48/places/default-folder-system.svg $papirus/48x48/places/folder-blue-development.svg
+sudo cp $dir/scalable/places/default-folder-system.svg $papirus/64x64/places/folder-blue-development.svg
 
 ## Templates
-sudo cp ./themes/Qogir-icon-theme/src/32/places/default-folder-templates.svg /usr/share/icons/Papirus/32x32/places/folder-blue-templates.svg
-sudo cp ./themes/Qogir-icon-theme/src/48/places/default-folder-templates.svg /usr/share/icons/Papirus/48x48/places/folder-blue-templates.svg
-sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-folder-templates.svg /usr/share/icons/Papirus/64x64/places/folder-blue-templates.svg
+sudo cp $dir/32/places/default-folder-templates.svg $papirus/32x32/places/folder-blue-templates.svg
+sudo cp $dir/48/places/default-folder-templates.svg $papirus/48x48/places/folder-blue-templates.svg
+sudo cp $dir/scalable/places/default-folder-templates.svg $papirus/64x64/places/folder-blue-templates.svg
 
 ## Videos
-sudo cp ./themes/Qogir-icon-theme/src/32/places/default-folder-video.svg /usr/share/icons/Papirus/32x32/places/folder-blue-video.svg
-sudo cp ./themes/Qogir-icon-theme/src/48/places/default-folder-video.svg /usr/share/icons/Papirus/48x48/places/folder-blue-video.svg
-sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-folder-video.svg /usr/share/icons/Papirus/64x64/places/folder-blue-video.svg
+sudo cp $dir/32/places/default-folder-video.svg $papirus/32x32/places/folder-blue-video.svg
+sudo cp $dir/48/places/default-folder-video.svg $papirus/48x48/places/folder-blue-video.svg
+sudo cp $dir/scalable/places/default-folder-video.svg $papirus/64x64/places/folder-blue-video.svg
 
-sudo cp ./themes/Qogir-icon-theme/src/32/places/default-network.svg /usr/share/icons/Papirus/32x32/places/folder-blue-network.svg
-sudo cp ./themes/Qogir-icon-theme/src/48/places/default-network.svg /usr/share/icons/Papirus/48x48/places/folder-blue-network.svg
-sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-network.svg /usr/share/icons/Papirus/64x64/places/folder-blue-network.svg
+sudo cp $dir/32/places/default-network.svg $papirus/32x32/places/folder-blue-network.svg
+sudo cp $dir/48/places/default-network.svg $papirus/48x48/places/folder-blue-network.svg
+sudo cp $dir/scalable/places/default-network.svg $papirus/64x64/places/folder-blue-network.svg
 
-sudo cp ./themes/Qogir-icon-theme/src/32/places/default-user-desktop.svg /usr/share/icons/Papirus/32x32/places/user-blue-desktop.svg
-sudo cp ./themes/Qogir-icon-theme/src/48/places/default-user-desktop.svg /usr/share/icons/Papirus/48x48/places/user-blue-desktop.svg
-sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-user-desktop.svg /usr/share/icons/Papirus/64x64/places/user-blue-desktop.svg
+sudo cp $dir/32/places/default-user-desktop.svg $papirus/32x32/places/user-blue-desktop.svg
+sudo cp $dir/48/places/default-user-desktop.svg $papirus/48x48/places/user-blue-desktop.svg
+sudo cp $dir/scalable/places/default-user-desktop.svg $papirus/64x64/places/user-blue-desktop.svg
 
 ## Symbolic
-sudo cp ./themes/Qogir-icon-theme/src/symbolic/places/* /usr/share/icons/Papirus-Dark/symbolic/places/
+sudo cp $dir/symbolic/places/* $papirusdark/symbolic/places/
 
 ## Recent Symbolic
-sudo cp ./themes/Qogir-icon-theme/src/symbolic/places/folder-recent-symbolic.svg /usr/share/icons/Papirus-Dark/symbolic/actions/document-open-recent-symbolic.svg
+sudo cp $dir/symbolic/places/folder-recent-symbolic.svg $papirusdark/symbolic/actions/document-open-recent-symbolic.svg
 
 ## Starred Symbolic
-sudo cp ./themes/Qogir-icon-theme/src/symbolic/places/user-bookmarks-symbolic.svg /usr/share/icons/Papirus-Dark/symbolic/status/starred-symbolic.svg
+sudo cp $dir/symbolic/places/user-bookmarks-symbolic.svg $papirusdark/symbolic/status/starred-symbolic.svg
 
 ## Directory Symbolic
-sudo cp ./themes/Qogir-icon-theme/src/symbolic/places/folder-symbolic.svg /usr/share/icons/Papirus-Dark/symbolic/mimetypes/inode-directory-symbolic.svg
+sudo cp $dir/symbolic/places/folder-symbolic.svg $papirusdark/symbolic/mimetypes/inode-directory-symbolic.svg
 
 ## Trash Symbolic
-sudo cp ./themes/Qogir-icon-theme/src/symbolic/places/user-trash-full-symbolic.svg /usr/share/icons/Papirus-Dark/symbolic/status/user-trash-full-symbolic.svg
+sudo cp $dir/symbolic/places/user-trash-full-symbolic.svg $papirusdark/symbolic/status/user-trash-full-symbolic.svg
 
 ## Mimetypes
 for i in 16 22 24 32 48 64
 do
-	folder=$i'x'$i
-	sudo cp ./themes/Qogir-icon-theme/src/scalable/mimetypes/* /usr/share/icons/Papirus/$folder/mimetypes/
-	sudo cp ./themes/Qogir-icon-theme/src/scalable/mimetypes/package-x-generic.svg /usr/share/icons/Papirus/$folder/apps/ark.svg
-	sudo cp ./themes/Qogir-icon-theme/src/scalable/mimetypes/text-x-script.svg /usr/share/icons/Papirus/$folder/mimetypes/application-json.svg
-	sudo cp ./themes/Qogir-icon-theme/src/scalable/mimetypes/text-x-generic.svg /usr/share/icons/Papirus/$folder/mimetypes/application-x-yaml.svg
-	sudo cp ./themes/Qogir-icon-theme/src/scalable/mimetypes/text-x-generic.svg /usr/share/icons/Papirus/$folder/mimetypes/text-x-generic.svg
-	sudo cp ./themes/Qogir-icon-theme/src/scalable/places/default-folder-publicshare.svg /usr/share/icons/Papirus/$folder/places/folder-blue-remote.svg
+	folder=${i}x${i}
+	sudo cp $dir/scalable/mimetypes/* $papirus/$folder/mimetypes/
+	sudo cp $dir/scalable/mimetypes/package-x-generic.svg $papirus/$folder/apps/ark.svg
+	sudo cp $dir/scalable/mimetypes/text-x-script.svg $papirus/$folder/mimetypes/application-json.svg
+	sudo cp $dir/scalable/mimetypes/text-x-generic.svg $papirus/$folder/mimetypes/application-x-yaml.svg
+	sudo cp $dir/scalable/mimetypes/text-x-generic.svg $papirus/$folder/mimetypes/text-x-generic.svg
+	sudo cp $dir/scalable/places/default-folder-publicshare.svg $papirus/$folder/places/folder-blue-remote.svg
 done
 
 # Arrow symbol replace
-sudo cp src/go-next-symbolic.svg /usr/share/icons/Papirus-Dark/symbolic/actions/go-next-symbolic.svg
-sudo cp src/go-previous-symbolic.svg /usr/share/icons/Papirus-Dark/symbolic/actions/go-previous-symbolic.svg
+sudo cp src/next.svg $papirusdark/symbolic/actions/go-next-symbolic.svg
+sudo cp src/previous.svg $papirusdark/symbolic/actions/go-previous-symbolic.svg
 
 # Applications
 # bash app.sh
