826287ed263c7ee216fa27042b26360a670291a0
Sparkvix <ev1lhat0l0ls@gmail.com>
2021-04-10 09:36:10 -0400

    Update tray.sh

--- tray.sh
+++ tray.sh
@@ -2,15 +2,15 @@
 
 # Swap some application trays
 
-sudo cp src/slacktray/Numix/slack16.svg /usr/share/icons/Papirus-Dark/16x16/panel/slack-indicator.svg
-sudo cp src/slacktray/Numix/slack22.svg /usr/share/icons/Papirus-Dark/22x22/panel/slack-indicator.svg
-sudo cp src/slacktray/Numix/slack24.svg /usr/share/icons/Papirus-Dark/24x24/panel/slack-indicator.svg
+sudo cp src/slack-tray/Numix/slack16.svg /usr/share/icons/Papirus-Dark/16x16/panel/slack-indicator.svg
+sudo cp src/slack-tray/Numix/slack22.svg /usr/share/icons/Papirus-Dark/22x22/panel/slack-indicator.svg
+sudo cp src/slack-tray/Numix/slack24.svg /usr/share/icons/Papirus-Dark/24x24/panel/slack-indicator.svg
 
 
-sudo cp src/slacktray/Numix/slack16r.svg /usr/share/icons/Papirus-Dark/16x16/panel/slack-indicator-unread.svg
-sudo cp src/slacktray/Numix/slack22r.svg /usr/share/icons/Papirus-Dark/22x22/panel/slack-indicator-unread.svg
-sudo cp src/slacktray/Numix/slack24r.svg /usr/share/icons/Papirus-Dark/24x24/panel/slack-indicator-unread.svg
+sudo cp src/slack-tray/Numix/slack16r.svg /usr/share/icons/Papirus-Dark/16x16/panel/slack-indicator-unread.svg
+sudo cp src/slack-tray/Numix/slack22r.svg /usr/share/icons/Papirus-Dark/22x22/panel/slack-indicator-unread.svg
+sudo cp src/slack-tray/Numix/slack24r.svg /usr/share/icons/Papirus-Dark/24x24/panel/slack-indicator-unread.svg
 
-sudo cp src/slacktray/Numix/slack16h.svg /usr/share/icons/Papirus-Dark/16x16/panel/slack-indicator-highlight.svg
-sudo cp src/slacktray/Numix/slack22h.svg /usr/share/icons/Papirus-Dark/22x22/panel/slack-indicator-highlight.svg
-sudo cp src/slacktray/Numix/slack24h.svg /usr/share/icons/Papirus-Dark/24x24/panel/slack-indicator-highlight.svg
+sudo cp src/slack-tray/Numix/slack16h.svg /usr/share/icons/Papirus-Dark/16x16/panel/slack-indicator-highlight.svg
+sudo cp src/slack-tray/Numix/slack22h.svg /usr/share/icons/Papirus-Dark/22x22/panel/slack-indicator-highlight.svg
+sudo cp src/slack-tray/Numix/slack24h.svg /usr/share/icons/Papirus-Dark/24x24/panel/slack-indicator-highlight.svg
